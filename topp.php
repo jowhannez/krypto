d7c2c5b6dfa47c1ad4fcdf799d7da79e4063c87a
Johannes Bratseth Håkonsen <jowhannez@hotmail.com>
2020-03-04 19:47:03 +0100

    added some styles

--- topp.php
+++ topp.php
@@ -1,17 +1,31 @@
+<style>
+	.container {
+		display: flex;
+	}
+	.block {
+		width: 50%;
+		padding: 10px;
+		border: solid 1px black;
+	}
+	.block__left {
+		border-right: none;
+	}
+</style>
+
 <?php
 	# topp.php
 	include "felles.php";
-	echo "STANDARD TOPP";
-	echo "<div>";
-	echo "<br> Velkommen til: " . PROGNAVN;
-	echo "</div>";
-	echo "<div>";
-	echo "Nyheter om kryptovalutaer:";
-	include "nyheter.html";
-	$filename = "nyheter.html";
-	if (file_exists($filename)) {
-		echo "Sist oppdatart: ".date ("F d Y H:i:s.", filemtime($filename));
-	 
-	}
-	echo "</div>";
+	echo "<section class='container'>";
+		echo "<div class='block block__left'>";
+			echo "<div>Velkommen til: " . PROGNAVN . "</div>";
+		echo "</div>";
+		echo "<div class='block'>";
+			echo "Nyheter om kryptovalutaer:";
+			include "nyheter.html";
+			$filename = "nyheter.html";
+			if (file_exists($filename)) {
+				echo "Sist oppdatart: ".date ("F d Y H:i:s.", filemtime($filename));
+			}
+		echo "</div>";
+	echo "</section>";
 ?>
